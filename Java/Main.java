4e55d8e0cd4abc9fb2dc56572e681e586231b0af
Jesús Rivera Flores <Jesus.RiveraF98@gmail.com>
2021-06-24 12:23:22 -0500

    Probar set pasajeros java

--- Java/Main.java
+++ Java/Main.java
@@ -4,7 +4,7 @@ class Main {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
         UberX uberX = new UberX("AMQ123", new Account("Andres Herrera", "AND123"), "Chevrolet", "Sonic");
-        //uberX.passengers = 3;
+        uberX.setPassengers(4);
         uberX.printDataCar();
 
         /*Car car2 = new Car("QWE567", new Account("Andrea Herrera", "AND567"));
