df93e36a8fe18b7ec210c1314269692465a87edd
Jesús Rivera Flores <Jesus.RiveraF98@gmail.com>
2021-06-24 11:30:36 -0500

    Agregar Ubers JS

--- JS/index.js
+++ JS/index.js
@@ -2,3 +2,9 @@ var car = new Car("AW456", new Account("Andres Herrera", "QWE234"))
 car.passenger = 4
 
 car.printDataCar()
+
+$uberX = new UberPool("QWE567", new Account("Andrea Ferran", "ANDA765"), "Chevrolet", "Spark");
+$uberX.passengers = 4;
+
+$uberX.printDataCar();
+
