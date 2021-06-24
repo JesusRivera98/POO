c2a04e200b1902f2e9dbb6510a1fa979764148f5
Jesús Rivera Flores <Jesus.RiveraF98@gmail.com>
2021-06-24 12:12:30 -0500

    Cambir car por uberx y agregar pasajeros a print java

--- Java/Car.java
+++ Java/Car.java
@@ -12,6 +12,6 @@ public class Car {
     }
 
     void printDataCar() {
-        System.out.println("License: " + license + " Driver: " + driver.name);
+        System.out.println("License: " + license + " Driver: " + driver.name + " Passengers: " + passengers);
     }
 }
--- Java/Main.java
+++ Java/Main.java
@@ -3,15 +3,13 @@ package CursoPOOUber.Java;
 class Main {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
-        Car car = new Car("AMQ123", new Account("Andres Herrera", "AND123"));
-        car.passengers = 4;
-        //System.out.println("Car License: " + car.license);
-        car.printDataCar();
+        UberX uberX = new UberX("AMQ123", new Account("Andres Herrera", "AND123"), "Chevrolet", "Sonic");
+        uberX.passengers = 4;
+        uberX.printDataCar();
 
-        Car car2 = new Car("QWE567", new Account("Andrea Herrera", "AND567"));
+        /*Car car2 = new Car("QWE567", new Account("Andrea Herrera", "AND567"));
         car2.passengers = 3;
-        //System.out.println("Car License: " + car2.license);
-        car.printDataCar();
+        car.printDataCar();*/
 
     }
 }
