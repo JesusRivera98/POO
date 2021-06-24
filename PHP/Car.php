78772574603c69cac6796f548c4f03f436851233
Jesús Rivera Flores <Jesus.RiveraF98@gmail.com>
2021-06-24 12:50:38 -0500

    Agregar get y set car PHP

--- PHP/Car.php
+++ PHP/Car.php
@@ -1,17 +1,35 @@
 <?php
-    require_once 'Account.php';
-    class Car {
-        public $id;
-        public $license;
-        public $driver;
-        public $passengers;
+require_once 'Account.php';
+class Car
+{
+    public $id;
+    public $license;
+    public $driver;
+    protected $passengers;
 
-        public function __construct($license, $driver){
-            $this->license = $license;
-            $this->driver = $driver;
+    public function __construct($license, $driver)
+    {
+        $this->license = $license;
+        $this->driver = $driver;
+    }
+
+    public function PrintDataCar()
+    {
+        echo "license: {$this->license}, driver: {$this->driver->name}";
+    }
+
+    public function getPassenger() {
+        return $this->passenger;
+    }
+
+    public function setPassenger($passenger) {
+        
+        if ($passenger == 4) {
+            $this->passenger = $passenger;
+        }
+        else {
+            echo "Necesitas asignar 4 pasajeros";
         }
     
-        public function PrintDataCar(){
-            echo "license: {$this->license}, driver: {$this->driver->name}";
-          }
     }
+}
