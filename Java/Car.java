b66c7033300b5bd579e01a38b53d839a04a4b1c3
Jesús Rivera Flores <Jesus.RiveraF98@gmail.com>
2021-06-24 12:34:32 -0500

    Agregar getter y setters Car java

--- Java/Car.java
+++ Java/Car.java
@@ -26,4 +26,30 @@ public class Car {
         else
             System.out.println("Necesitas asignar 4 pasajeros");
     }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getLicense() {
+        return license;
+    }
+
+    public void setLicense(String license) {
+        this.license = license;
+    }
+
+    public Account getDriver() {
+        return driver;
+    }
+
+    public void setDriver(Account driver) {
+        this.driver = driver;
+    }
+
+    
 }
